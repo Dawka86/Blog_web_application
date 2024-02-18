6eeabf6dd85bd672d2c1d416f1c8e9cd6c986203
Dawid <105428376+Dawka86@users.noreply.github.com>
2024-02-18 12:32:39 +0000

    Update

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ import express from "express";
 
 const app = express();
 const port = 3000;
+app.use(express.static("public"));
 
 app.get("/", (req, res)=>{
     res.render("index.ejs");
 })
 app.get("/about",(req, res)=>{
     res.render("about.ejs");
+})
+app.get("/blog", (req,res)=>{
+    res.render("blog.ejs");
 })
 
 
