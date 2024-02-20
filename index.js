6b3b5b1b094fec679f62bd45a9f117f3ed90c586
Dawid <105428376+Dawka86@users.noreply.github.com>
2024-02-20 18:27:56 +0000

    update

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res)=>{
     res.render("index.ejs");
@@ -13,6 +16,22 @@ app.get("/about",(req, res)=>{
 app.get("/blog", (req,res)=>{
     res.render("blog.ejs");
 })
+
+
+app.post("/blog/submit", (req,res)=>{
+    const name = req.body["Fname"];
+    const surname = req.body["LastName"];
+    const email = req.body["exampleInputEmail1"];
+    const massage = req.body["textarea"];
+    
+    console.log(name);
+    console.log(surname);
+    console.log(email);
+    console.log(massage);
+
+})
+
+
 
 
 
